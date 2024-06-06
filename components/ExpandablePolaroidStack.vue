281d4ddc7d500765e48bf4b20341e8424c0c4fb8
Steffen Linßen <linssenste@gmail.com>
2024-06-06 12:38:47 +0200

    created expandable polaroid-themed stack

---
+++ components/ExpandablePolaroidStack.vue
@@ -0,0 +1,267 @@
+<template>
+	<div id="polaroid-stack">
+
+		<!-- stack animation section -->
+		<ul class="polaroids-list" ref="listRef">
+
+			<!-- polaroid images stack -->
+			<li v-for="(card, index) in polaroids" :key="index" @click="toggleExpansion()" :style="cardStyling(index)"
+				class="polaroid " :data-testid="`polaroid-card-${index}`">
+				<PolaroidImage :src="card.src" :alt="card.alt" :id="`polaroid-${index}`" />
+			</li>
+
+			<!-- appended close button -->
+			<li class="polaroid append-slot" :style="cardStyling(polaroids.length)"
+				:class="!expanded ? 'append-slot-hidden' : ''" data-testid="close-button-card">
+
+				<button v-on:click="toggleExpansion" data-testid="close-button">
+					<img src="../assets/icons/chevron-left.svg" alt="chevron left" title="Close polaroid stack"
+						 width="16" height="16" /></button>
+			</li>
+		</ul>
+
+
+		<!-- text section -->
+		<div class="welcome-text-area" :style="textTransition">
+			<div class="intro-text">
+				<slot name="text" />
+			</div>
+		</div>
+
+	</div>
+</template>
+
+<script lang="ts" setup>
+
+export interface POLAROID_CARD {
+	src: string,
+	alt: string
+}[]
+
+const props = defineProps<{
+	polaroids: POLAROID_CARD[];
+}>();
+
+props.polaroids;
+
+const showText = ref(true); // hide text part if expanded
+const expanded = ref(false);
+
+// references
+const listRef = ref<HTMLElement | null>(null);
+
+const windowWidth = ref(0);
+const isVisible = ref(true)
+onMounted(() => {
+
+	windowWidth.value = window.innerWidth;
+
+	if (!listRef.value) return;
+	const observer = new IntersectionObserver(
+		([entry]) => {
+			if (!entry.isIntersecting) {
+				isVisible.value = false
+				if (expanded.value) toggleExpansion();
+			} else {
+				isVisible.value = true
+			}
+		},
+		{ threshold: 0 }
+	);
+	observer.observe(listRef.value);
+
+	setStyling();
+
+	window.addEventListener('resize', handleResize);
+
+});
+
+
+function handleResize() {
+	windowWidth.value = window.innerWidth;
+	if (expanded.value) {
+		toggleExpansion();
+	}
+}
+onBeforeUnmount(() => {
+	window.removeEventListener('resize', handleResize);
+});
+
+const textTransition = computed(() => {
+	return (windowWidth.value >= 1000 && !showText.value ? 'width: 0px!important; transition: all .3s ease-in-out' : 'transition: all .15s ease-in-out')
+})
+
+function toggleExpansion(): void {
+
+	expanded.value = !expanded.value;
+
+	if (window.innerWidth <= 850) showText.value = true;
+	else showText.value = !expanded.value
+
+	if (listRef.value) setStyling()
+}
+
+defineExpose({ toggleExpansion })
+
+function setStyling() {
+
+	if (!listRef.value) return;
+
+	// editable min and max width of stacked polaroids 
+	const minWidth = "380px"
+	const maxWidth = "calc(100% - 30px)";
+
+	if (!expanded.value) listRef.value.style.overflowX = 'hidden'
+	else listRef.value.style.overflowX = 'scroll'
+
+	// listRef.value.style.maxWidth = expanded.value ? maxWidth : minWidth;
+	listRef.value.style.transition = `all ${expanded.value ? '.3' : '.3'}s ease-in-out`;
+	listRef.value.style.width = expanded.value || window.innerWidth < 380 ? maxWidth : minWidth;
+}
+
+
+function cardStyling(index: number) {
+	return {
+		zIndex: 10,
+		transform: `rotateZ(${(expanded.value || windowWidth.value == 0 || !isVisible.value ? 0 : Math.floor(Math.random() * 15 - 7))}deg)`,
+		left: `${(expanded.value ? index * 345 + 28 : 28)}px`,
+	};
+}
+
+</script>
+
+<style scoped>
+#polaroid-stack {
+	position: relative;
+	display: flex;
+
+	min-height: 500px;
+	flex-direction: row;
+
+	align-items: center;
+	justify-content: center;
+	margin-bottom: 15px;
+}
+
+
+
+.welcome-text-area {
+	position: relative;
+	overflow: hidden;
+	width: 700px;
+	max-width: calc(100% - 450px);
+}
+
+.intro-text {
+	position: relative;
+	width: 550px;
+	max-width: 550px;
+	min-width: 550px;
+	text-align: justify;
+}
+
+@media screen and (max-width: 1000px) {
+	#polaroid-stack {
+
+		gap: 10px;
+		flex-direction: column;
+
+	}
+
+	.welcome-text-area {
+		width: 100%;
+		max-width: 100%;
+	}
+
+	.intro-text {
+
+		position: relative;
+		width: calc(100% - 40px) !important;
+		max-width: calc(100% - 40px);
+		min-width: calc(100% - 40px);
+		padding-left: 0px;
+		padding: 20px;
+		padding-top: 0px;
+	}
+
+}
+
+ul.polaroids-list {
+	position: relative;
+
+	height: 460px;
+	min-width: 380px;
+	padding-top: 40px;
+
+	margin: 0px !important;
+
+	list-style-type: none;
+	white-space: nowrap;
+	cursor: default;
+
+	user-select: none;
+	-moz-user-select: none;
+	-webkit-user-select: none;
+
+	overflow-y: hidden;
+	overflow-x: auto;
+
+
+}
+
+
+ul.polaroids-list li.polaroid {
+
+	position: absolute;
+	overflow: hidden;
+	cursor: pointer;
+	transition: all .3s ease-in-out;
+	-webkit-transition: all .3s ease-in-out;
+	-moz-transition: all .3s ease-in-out;
+}
+
+
+
+.append-slot {
+	transition: all 50ms ease-in-out;
+	position: relative;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+
+	width: 50px;
+	height: 400px;
+
+	opacity: 1;
+	padding-right: 40px;
+	padding-left: 20px;
+	padding-top: 10px
+}
+
+.append-slot button {
+	background-color: #e0e0e0;
+	border-radius: 10px;
+
+	outline: none;
+	border: none;
+	height: 100px;
+	width: 50px;
+	opacity: .65;
+	cursor: pointer;
+	transition: all 200ms ease-in-out;
+}
+
+.append-slot button:hover {
+	height: 400px;
+	opacity: 1;
+}
+
+
+.append-slot-hidden {
+	width: 0px !important;
+	opacity: 0;
+
+	padding-right: 0px !important;
+	pointer-events: none
+}
+</style>
---
+++ components/PolaroidImage.vue
@@ -0,0 +1,48 @@
+<template>
+
+	<div class="polaroid-image" ref="polaroidRef">
+		<img :src="src" width="100%" height="100%" :alt="alt" data-testid="image" draggable="false" />
+	</div>
+
+</template>
+
+<script lang="ts" setup>
+import VanillaTilt from 'vanilla-tilt'
+
+const polaroidRef = ref<HTMLElement | null>(null);
+
+const props = defineProps<{
+	src: string;
+	alt: string
+}>();
+
+props.src;
+props.alt;
+
+
+onMounted(() => {
+	if (!polaroidRef.value) return;
+
+	// initalizing tilt functionality (only on non-touch devices)
+	VanillaTilt.init(polaroidRef.value, {
+		max: 5,
+		speed: 1000,
+		reverse: true,
+
+		glare: true,
+		gyroscope: false,
+		"max-glare": .15,
+	})
+})
+
+</script>
+
+
+<style scoped>
+.polaroid-image {
+	position: relative;
+	width: 335px;
+	margin: 10px;
+	height: 401px
+}
+</style>
