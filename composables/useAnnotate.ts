24d1e9e2f9de6b1ba006b6cf541a9ff1ec9d2902
Steffen Linßen <linssenste@gmail.com>
2024-06-09 22:56:52 +0200

    updated annotation to be triggerable

--- composables/useAnnotate.ts
+++ composables/useAnnotate.ts
@@ -2,24 +2,46 @@ import { annotate } from 'rough-notation';
 
 type Annotation = ReturnType<typeof annotate>;
 
-export function useAnnotate(elementId: string) {
+export function useAnnotate(elementId: string, toggle?: Ref<boolean>, lineType = 'underline', delay = 0) {
   const observer = ref<IntersectionObserver | null>(null);
   const annotation = ref<Annotation | null>(null);
 
-  onMounted(() => {
+  const setupAnnotation = () => {
     const humanText = document.getElementById(elementId);
     if (!humanText) return;
-    
-    annotation.value = annotate(humanText, { type: 'underline', color: 'var(--pen-color)', padding: [0, 3, 0, 3] });
 
-    observer.value = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) annotation.value?.show();
+    annotation.value = annotate(humanText, {
+      type: lineType,
+      color: 'var(--pen-color)',
+      padding: lineType == 'underline' ? [0, 3, 0, 3] : [5, 5],
+    });
+
+    if (toggle) {
+      // Use toggle variable to show/hide annotation
+      watch(toggle, (newValue) => {
+
+        if (newValue) {
+			setTimeout(() => {
+				annotation.value?.show();
+			}, delay);
+		}
         else annotation.value?.hide();
-      },
-      { threshold: 0 }
-    );
-    observer.value.observe(humanText);
+      });
+    } else {
+      // Use IntersectionObserver to show/hide annotation based on visibility
+      observer.value = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) annotation.value?.show();
+          else annotation.value?.hide();
+        },
+        { threshold: 0 }
+      );
+      observer.value.observe(humanText);
+    }
+  };
+
+  onMounted(() => {
+    setupAnnotation();
   });
 
   onUnmounted(() => {
