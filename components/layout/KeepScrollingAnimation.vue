95acffd8f646fddf05e844583047f5191fdfb661
Steffen Linßen <linssenste@gmail.com>
2024-07-16 16:16:07 +0200

    reduced animation distance + speed for scrolling

--- components/layout/KeepScrollingAnimation.vue
+++ components/layout/KeepScrollingAnimation.vue
@@ -2,14 +2,22 @@
 	<div class="hint-area" oncontextmenu="return false">
 		<div class="scroll-downs">
 			<div class="mousey">
-				<div class="scroller"></div>
+				<div class="scroller" v-if="!isClick"></div>
+				<div class="clicker" v-else></div>
 			</div>
 		</div>
-
-		KEEP SCROLLING
+		{{ text }}
 	</div>
 </template>
 
+<script lang="ts" setup>
+
+const props = defineProps<{
+	isClick: boolean,
+	text: string
+}>();
+
+</script>
 
 <style scoped>
 .hint-area {
@@ -23,11 +31,9 @@
 	text-transform: uppercase;
 	color: var(--red-color);
 	margin-bottom: 15px;
-
 	user-select: none;
 	-moz-user-select: none;
 	-webkit-user-select: none;
-
 }
 
 .scroll-downs {
@@ -41,6 +47,7 @@
 	border: 3px solid var(--red-color);
 	border-radius: 25px;
 	box-sizing: content-box;
+	position: relative;
 }
 
 .scroller {
@@ -55,25 +62,50 @@
 	animation-iteration-count: infinite;
 }
 
+.clicker {
+	width: 5px;
+	height: 5px;
+	background-color: var(--red-color);
+	border-radius: 50%;
+	position: absolute;
+	top: 7px;
+	left: 7px;
+	transform: translate(-50%, -50%);
+	animation: click 750ms infinite;
+}
+
 @keyframes scroll {
+
 	0% {
 		transform: translateY(0);
 	}
 
-	10% {
-		transform: translateY(0);
-
+	50% {
+		transform: translateY(7px);
 	}
 
-
-
-
-	95% {
-		transform: translateY(13px);
-	}
-
-	100% {
+	75% {
 		transform: translateY(0);
 	}
 }
+
+@keyframes click {
+
+	0%,
+	100% {
+		transform: translate(-50%, -50%) scale(1);
+	}
+
+	50% {
+		transform: translate(-50%, -50%) scale(0.8);
+	}
+}
+
+/* .mousey.click {
+	padding: 0;
+	border-radius: 50%;
+	width: 20px;
+	height: 20px;
+	border: 3px solid var(--red-color);
+} */
 </style>
