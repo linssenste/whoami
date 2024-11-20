dc9be7ab07c37d1b124629904dcaf364b030d8aa
Steffen Linßen <linssenste@gmail.com>
2024-11-20 12:56:48 +0100

    [feature] created section for instax-link-web embed area

--- app.vue
+++ app.vue
@@ -1,40 +1,24 @@
 <template>
 	<div id="app">
 
-
-
-
 		<JumpTopButton />
 
 
 		<WelcomeSection class="welcome-section" style="width: 100%; overflow-x: hidden;" id="welcome-section" />
-		<LayoutToolbar :playing="isPlaying" />
-		<MusicSection id="analysis-section" v-on:playing="isPlaying = $event" style="z-index: 10;" />
+		<LayoutToolbar />
+		<MusicSection id="analysis-section" style="z-index: 10; " />
 		<TravelingSection id="travel-section" />
 
-		<div id="polaroid-section"
-			 style="height: calc(100vh - 40px); width: calc(100% - 40px); margin: 20px; margin-top: 10 0px; border-radius: 20px; overflow: hidden; position: relative;">
-
-			<iframe width="100%" height="100%" style="border: none" src="https://instax.linssenste.dev" />
-		</div>
+		<PolaroidSection id="polaroid-section" />
 
 
-		<h1>* this is a side project of mine</h1>
-
-		<div id="other-section" style="width: 100%; height: 900px; background-color: grey;"></div>
-
-
-
-
-
-
+		<div id="facts-section" style="width: 100%; height: 900px; background-color: grey;"></div>
 
 	</div>
 </template>
 
 <script lang="ts" setup>
 
-const isPlaying = ref(false)
 useHead({
 	title: "Steffen's Portfolio",
 	htmlAttrs: {
@@ -47,6 +31,8 @@ useHead({
 		}
 	]
 });
+
+
 </script>
 
 <style scoped>
---
+++ sections/PolaroidSection.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="polaroid-section-container">
+		<div class="polaroid-generator-embed" :class="{ 'embed-mobile': isMobile }">
+			<iframe width="100%" height="100%" style="border: none" src="https://instax.linssenste.dev" />
+		</div>
+
+		<!-- Place this tag where you want the button to render. -->
+		<github-button href="https://github.com/linssenste/instax-link-web"
+					   data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large"
+					   data-show-count="true"
+					   aria-label="Star linssenste/instax-link-web on GitHub">Star</github-button>
+		<h1>* this is a side project of mine</h1>
+	</div>
+</template>
+
+
+
+<script lang="ts" setup>
+import GithubButton from 'vue-github-button'
+
+const { isMobile } = useDeviceDetection();
+</script>
+<style scoped>
+.polaroid-section-container {
+	position: relative;
+	height: fit-content;
+}
+
+.polaroid-generator-embed {
+	height: calc(100vh - 40px);
+	width: calc(100% - 40px);
+	margin: 20px;
+	margin-top: 10 0px;
+	border-radius: 20px;
+	overflow: hidden;
+	position: relative;
+}
+
+.embed-mobile {
+	height: calc(100vh - 20px);
+	width: 100%;
+	border-radius: 0px;
+	margin: 0px !important;
+	margin-top: 10px;
+}
+</style>
